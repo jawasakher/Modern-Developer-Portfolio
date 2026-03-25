a03c4d84658c1ab8a9de00286383ed01501ffbf1
jawasakher <jawasakher@example.com>
2026-03-25 22:07:34 +0300

    staged changes app.jsx navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Navbar} from './components/layout/Navbar'
-import {Hero }from './components/sections/Hero'
+import Navbar from './components/layout/Navbar'
+import Hero from './components/sections/Hero'
 const App = () => {
   return (
     <div className='min-h-screen bg-black'>
